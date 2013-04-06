5590aaf33a94d049f9b0b4a0d9aa4fe84e14980e
Braxton Huggins <braxton@delorum.com>
2013-04-06 17:27:22 -0600

    more updates

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -2,6 +2,7 @@
 <head>
 </head>
 <body>
+  <p><a href="log_out">log out</a></p>
   <h1>Welcome <?php echo $first_name . " " . $last_name ?> from ward <?php echo $ward_name ?></h1>
   <p>Your ward has a goal of getting <?php echo $ward_goal ?> points.</p>
   <h2>Events</h2>
