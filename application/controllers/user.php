32b8b7be3081102dc0ddf05657940bdf708d1ca5
Braxton Huggins <braxton@delorum.com>
2013-04-06 13:24:51 -0600

    fix my typo

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -46,7 +46,7 @@ class User extends CI_Controller {
     elseif ($this->input->server('REQUEST_METHOD') == 'GET')
     {
       $this->load->model('Ward_model');
-      $data['wards'] = $this->Ward->get_wards();
+      $data['wards'] = $this->Ward_model->get_wards();
       $this->load->view('user/new');
     }
   }
