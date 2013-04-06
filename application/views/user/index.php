0bddb340940b9d86cdde555a543d0b6e4e663b3f
Braxton Huggins <braxton@delorum.com>
2013-04-06 17:06:31 -0600

    more updates

---
+++ application/views/user/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+  <h1>Welcome <?php echo $first_name . " " . $last_name ?> from ward <?php echo $ward_name ?></h1>
+  <p>Your ward has a goal of getting <?php echo $ward_goal ?> points.</p>
+  <h2>Events</h2>
+  <h2>Batches</h2>
+</body>
+</html>
