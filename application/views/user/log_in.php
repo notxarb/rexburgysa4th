c2e6666be989b7daa25f0b64a78ae00ee3a646c4
Braxton Huggins <braxton@delorum.com>
2013-04-06 16:48:18 -0600

    more updates

--- application/views/user/log_in.php
+++ application/views/user/log_in.php
@@ -3,7 +3,7 @@
   </head>
   <body>
     <form action='log_in' method='post'>
-      Username: <input type="text" name="last_name"><br>
+      Username: <input type="text" name="user_name"><br>
       Password: <input type="password" name="password"><br>
       <input type="submit" value="Submit">
     </form>
