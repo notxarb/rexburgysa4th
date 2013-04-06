fae8e8f87c199f21ccb52214431e9b54827135e8
Braxton Huggins <braxton@delorum.com>
2013-04-06 13:01:37 -0600

    adding lots of stuff

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,7 +19,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->model('Ward');
+		$this->load->model('Ward_model');
 		$data["wards"] = $this->Ward->get_wards();
 		$this->load->view('welcome_message');
 	}
