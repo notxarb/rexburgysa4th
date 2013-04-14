ac7b05e6ce4b201043f9701df3483026b336cad5
Braxton Huggins <braxton@delorum.com>
2013-04-13 20:17:00 -0600

    bugfix

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -64,21 +64,23 @@ class User_model extends CI_Model {
     $batches = $query->result();
     foreach($events as $event)
     {
-      // if (($event->date >= $begin_time) && ($event->date <= $end_time))
-      // {
-        $week = floor(ceil(abs($event->date - strtotime('2013-03-24')) / 86400) / 7);
-        $day = floor(ceil(abs($event->date - strtotime('2013-03-24')) / 86400) % 7);
+      $date = strtotime($event->date)
+      if (($date>= $begin_time) && ($date <= $end_time))
+      {
+        $week = floor(ceil(abs($date - strtotime('2013-03-24')) / 86400) / 7);
+        $day = floor(ceil(abs($date - strtotime('2013-03-24')) / 86400) % 7);
         $calendar[$week][$day]['events'][] = clone $event;
-      // }
+      }
     }
     foreach($batches as $batch)
     {
-      // if (($event->date >= $begin_time) && ($event->date <= $end_time))
-      // {
-        $week = floor(ceil(abs($batch->date - strtotime('2013-03-24')) / 86400) / 7);
-        $day = floor(ceil(abs($batch->date - strtotime('2013-03-24')) / 86400) % 7);
+      $date = strtotime($batch->date)
+      if (($date>= $begin_time) && ($date <= $end_time))
+      {
+        $week = floor(ceil(abs($date - strtotime('2013-03-24')) / 86400) / 7);
+        $day = floor(ceil(abs($date - strtotime('2013-03-24')) / 86400) % 7);
         $calendar[$week][$day]['batches'][] = clone $batch;
-      // }
+      }
     }
     return $calendar;
   }
